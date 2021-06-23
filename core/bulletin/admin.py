6da970f90104b46030100c03f33ef8082d639236
Samkelo Kgosiemang <joymama078@gmail.com>
2021-06-23 10:12:08 +0200

    Create bulletin model

--- core/bulletin/admin.py
+++ core/bulletin/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
-# Register your models here.
+from core.bulletin.models import (
+    Bulletin,
+    BulletinImage,
+)
+
+class BulletinImageInline(admin.TabularInline):
+    model = BulletinImage
+    extra = 3
+
+class BulletinAdmin(admin.ModelAdmin):
+    inlines = [ BulletinImageInline, ]
+
+admin.site.register(Bulletin, BulletinAdmin)
--- core/bulletin/models.py
+++ core/bulletin/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+from core.accounts.models import User
+
+
+class Bulletin(models.Model):
+    object_id = models.CharField(max_length=11, null=False, blank=False)
+    user = models.ForeignKey(User, null=True, on_delete=models.DO_NOTHING)
+    caption = models.TextField(null=True, blank=True)
+    
+class BulletinImage(models.Model):
+    property = models.ForeignKey(
+        Bulletin, 
+        on_delete=models.CASCADE, 
+        related_name='images'
+    )
+    image = models.ImageField(upload_to='bulletin/images/')
