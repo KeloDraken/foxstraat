2a61409f9265ef9d60123008b12ac7172be09315
Samkelo Kgosiemang <joymama078@gmail.com>
2021-06-23 12:15:09 +0200

    Add selected image preview to post creation form

---
+++ utils/db.py
@@ -0,0 +1,34 @@
+import random
+import string
+
+
+def object_id_generator(size, model, chars=string.ascii_letters + string.digits):
+    '''
+    Generates and returns base64 call id
+    '''
+    object_id = ''.join(random.choice(chars) for _ in range(size))
+
+    return check_object_id_exists(object_id=object_id, model=model)
+
+def check_object_id_exists(object_id, model):
+    '''
+    Checks if call id exists. Generates and returns new call id if exists
+    '''
+    # Try/Catch checks to see if a Submission with object_id == object_id
+    # already exists. This will not throw an error if True. And if
+    # that's the case, the function becomes recursive until a unique
+    # object_id is generated
+    try:
+        model.objects.get(object_id=object_id)
+
+        # New object_id generated
+        new_object_id = object_id_generator()
+
+        # New object_id checked against all Submission objects
+        check_object_id_exists(object_id=new_object_id, model=model)
+
+    except:
+        # This means there has been an error which means
+        # that there's no Submission object with object_id == object_id
+        return object_id
+
