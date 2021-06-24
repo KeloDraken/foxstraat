23c7b1620643ff96e0f032944078f01894514e4c
Samkelo Kgosiemang <joymama078@gmail.com>
2021-06-24 23:06:44 +0200

    Add bulletin title field to bulletin model

--- core/bulletin/models.py
+++ core/bulletin/models.py
@@ -9,6 +9,7 @@ from core.accounts.models import User
 class Bulletin(models.Model):
     object_id = models.CharField(max_length=11, null=False, blank=False)
     user = models.ForeignKey(User, null=True, on_delete=models.DO_NOTHING)
+    title = models.CharField(max_length=140, null=False, blank=False)
     caption = models.TextField(null=True, blank=True)
     date_created = models.DateField(auto_now_add=True, null=True, blank=True)
     datetime_created = models.DateTimeField(auto_now_add=True, null=True, blank=True)
