8615f383ee422967f9797a1d7023966dbd8a8af7
Samkelo Kgosiemang <joymama078@gmail.com>
2021-06-30 13:07:31 +0200

    Create topic and category models

--- core/forums/models.py
+++ core/forums/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    object_id = models.CharField(max_length=11, null=False, blank=False)
+    name = models.CharField(max_length=100, null=False, blank=False)
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class Topic(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    object_id = models.CharField(max_length=11, null=False, blank=False)
+    name = models.CharField(max_length=100, null=False, blank=False)
+    url_string = models.CharField(max_length=100, null=False, blank=False)
+    about = models.CharField(max_length=240, null=True, blank=True)
+    
+    
