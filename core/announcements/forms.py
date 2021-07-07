0594a3d64a7c6efc171b5c80477f6c18aa6e31b2
Samkelo Kgosiemang <joymama078@gmail.com>
2021-07-07 19:25:44 +0200

    Add classes and other attrs to annoucement creation form

--- core/announcements/forms.py
+++ core/announcements/forms.py
@@ -3,6 +3,27 @@ from core.announcements.models import Announcement
 
 
 class AnnouncementCreationForm(forms.ModelForm):
+    title = forms.CharField(
+        max_length=140, 
+        label='', 
+        widget=forms.TextInput(
+            attrs={
+                'class': 'form-input',
+                'placeholder': 'Give the announcement a short title',
+                'autocomplete': 'off',
+                'autofocus': 'true',
+                'autocapitalize': 'off'
+            }
+        )
+    ) 
+    body = forms.CharField(required=False, widget=forms.Textarea(
+        attrs={
+            'cols': 100,
+            'class': 'form-input',
+            'placeholder': 'Write the announcement body...',
+            'rows': 100,
+        }
+    ))
     class Meta:
         model = Announcement
         fields = (
