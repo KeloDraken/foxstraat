c55ee170c8db519bd4a611d326c2d91a25b57cd0
Samkelo Kgosiemang <joymama078@gmail.com>
2021-07-07 20:42:42 +0200

    Begin working on search processor

--- core/search/views.py
+++ core/search/views.py
@@ -1,3 +1,14 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
-# Create your views here.
+
+@login_required
+def search(request):
+    try:
+        query = request.GET['query']
+        context = {
+            'query': query
+        }
+        return render(request, 'views/search/results.html', context)
+    except:
+        return render(request, 'views/search/search.html')
