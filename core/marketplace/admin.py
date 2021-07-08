3f0ee10713f11cd1e3a9e8029cd62fb389d483ad
Samkelo Kgosiemang <joymama078@gmail.com>
2021-07-08 18:24:51 +0200

    Create marketplace template

--- core/marketplace/admin.py
+++ core/marketplace/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from core.marketplace.models import Template
 
-# Register your models here.
+
+admin.site.register(Template)
--- core/marketplace/models.py
+++ core/marketplace/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from imagekit.models import ProcessedImageField
+from imagekit.processors import ResizeToFit
 
-# Create your models here.
+class Template(models.Model):
+    object_id = models.CharField(max_length=11, null=False, blank=False)
+    name = models.CharField(max_length=140, null=False, blank=False)
+    description = models.CharField(max_length=280, null=False, blank=False)
+    screenshot_1 = ProcessedImageField(
+        upload_to='marketplace/templates/',
+        processors=[ResizeToFit(480, 600)],
+        format='JPEG',
+        options={'quality': 90},
+        null=False,
+        blank=False,
+    )
+    screenshot_2 = ProcessedImageField(
+        upload_to='marketplace/templates/',
+        processors=[ResizeToFit(480, 600)],
+        format='JPEG',
+        options={'quality': 90},
+        null=False,
+        blank=False,
+    )
+    price = models.PositiveIntegerField(default=0)
+    template = models.TextField(null=False, blank=False)
