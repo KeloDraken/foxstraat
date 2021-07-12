5b1a2980b5854febb6bcaac5339f73086840889c
Samkelo Kgosiemang <joymama078@gmail.com>
2021-07-12 17:22:44 +0200

    Add ref admin

--- core/admin.py
+++ core/admin.py
@@ -1,4 +1,11 @@
 from django.contrib import admin
 from core.models import Ref
 
-admin.site.register(Ref)
+
+class RefAdmin(admin.ModelAdmin):
+    list_display = (
+        'source',
+        'hits',
+    )
+
+admin.site.register(Ref, RefAdmin)
