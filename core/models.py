c79bdc84b2cb2136daf23f063c4830d0badb4fa3
Samkelo Kgosiemang <joymama078@gmail.com>
2021-07-12 18:28:34 +0200

    Add object to Ref model

--- core/models.py
+++ core/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 
 class Ref(models.Model):
+    object_id = models.CharField(max_length=11, null=True, blank=True)
     source = models.CharField(max_length=200, null=False, blank=False)
     hits = models.PositiveIntegerField(default=0)
 
