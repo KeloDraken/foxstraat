38d6efa5da0d3bbd17919a32bad5d7b571592db8
Samkelo Kgosiemang <joymama078@gmail.com>
2021-07-14 19:06:45 +0200

    Create music charts route

--- core/bulletin/urls.py
+++ core/bulletin/urls.py
@@ -8,6 +8,7 @@ from core.bulletin.views import (
     get_bulletin,
     get_song,
     manage_posts,
+    music_chart,
 )
 
 
@@ -15,10 +16,15 @@ app_name = 'bulletin'
 
 urlpatterns = [
     path('add/', create_bulletin, name='create-bulletin'),
-    path('add/song/', add_song, name='add-song'),
     path('manage/', manage_posts, name='manage-posts'),
     path('delete/<bulletin_id>', delete_post, name='delete-post'),
+
+    # Music
+    path('add/song/', add_song, name='add-song'),
     path('song/<song_id>/', get_song, name='get-song'),
+    path('song/<song_id>/', get_song, name='get-song'),
+    path('charts/', music_chart, name='music-charts'),
+    
     path('<bulletin_id>/', get_bulletin, name='get-bulletin'),
     path('', explore_bulletins, name='explore-bulletins'),
 ]
