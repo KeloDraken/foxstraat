a7166795e7adaa7301046c2f9b52701de0a55d35
Samkelo Kgosiemang <joymama078@gmail.com>
2021-07-14 18:03:10 +0200

    Add view song view + template

--- core/bulletin/urls.py
+++ core/bulletin/urls.py
@@ -6,6 +6,7 @@ from core.bulletin.views import (
     delete_post, 
     explore_bulletins,
     get_bulletin,
+    get_song,
     manage_posts,
 )
 
@@ -17,6 +18,7 @@ urlpatterns = [
     path('add/song/', add_song, name='add-song'),
     path('manage/', manage_posts, name='manage-posts'),
     path('delete/<bulletin_id>', delete_post, name='delete-post'),
+    path('song/<song_id>/', get_song, name='get-song'),
     path('<bulletin_id>/', get_bulletin, name='get-bulletin'),
     path('', explore_bulletins, name='explore-bulletins'),
 ]
--- core/bulletin/views.py
+++ core/bulletin/views.py
@@ -2,7 +2,7 @@ from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 
 from django.http.response import Http404
-from django.shortcuts import redirect, render
+from django.shortcuts import get_object_or_404, redirect, render
 
 from utils.helpers import (
     extract_hashtags,
@@ -102,7 +102,7 @@ def add_song(request):
                 post_form.user = request.user
 
                 if request.FILES.get('cover_art'):
-                    post_form.image = request.FILES.get('cover_art')
+                    post_form.cover_art = request.FILES.get('cover_art')
                 else:
                     messages.error(request, 'Song wasn\'t added. No cover art was found')
                     return redirect('bulletin:add-song')
@@ -124,7 +124,7 @@ def add_song(request):
                     request.user.save()
                     post_form.save()
                     
-                    return redirect(f'/p/{object_id}')
+                    return redirect(f'/p/song/{object_id}')
 
             else:    
                 messages.error(request, 'Post creation failed')
@@ -149,7 +149,7 @@ def add_song(request):
 
 def get_bulletin(request, bulletin_id):
     ref_from_url(request)
-    post = Bulletin.objects.get(object_id=bulletin_id)
+    post = get_object_or_404(Bulletin, object_id=bulletin_id)
 
     if request.user.is_authenticated:
         post.upvotes += 1
@@ -166,6 +166,28 @@ def get_bulletin(request, bulletin_id):
     return render(
         request, 
         'views/bulletin/view_bulletin.html', 
+        context
+    )
+
+def get_song(request, song_id):
+    ref_from_url(request)
+    post = get_object_or_404(Song, object_id=song_id)
+
+    if request.user.is_authenticated:
+        post.upvotes += 1
+        post.save()
+
+    more_from_user = Song.objects.filter(
+        user=post.user
+    ).order_by('?')[:2]
+
+    context = {
+        'post': post,
+        'more_from_user': more_from_user
+    }
+    return render(
+        request, 
+        'views/bulletin/view_song.html', 
         context
     )
 
