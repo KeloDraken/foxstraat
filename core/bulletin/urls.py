e272103d877553ffe69935891a25abcfff353eb3
Samkelo Kgosiemang <joymama078@gmail.com>
2021-07-14 17:25:38 +0200

    Create add song route

--- core/bulletin/urls.py
+++ core/bulletin/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 
 from core.bulletin.views import (
+    add_song,
     create_bulletin,
     delete_post, 
     explore_bulletins,
@@ -13,6 +14,7 @@ app_name = 'bulletin'
 
 urlpatterns = [
     path('add/', create_bulletin, name='create-bulletin'),
+    path('add/song/', add_song, name='add-song'),
     path('manage/', manage_posts, name='manage-posts'),
     path('delete/<bulletin_id>', delete_post, name='delete-post'),
     path('<bulletin_id>/', get_bulletin, name='get-bulletin'),
