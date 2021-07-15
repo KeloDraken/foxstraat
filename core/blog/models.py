09a8030787a10b1294edc9063a78ba7fa58aaf4c
Samkelo Kgosiemang <joymama078@gmail.com>
2021-07-15 22:09:10 +0200

    Create Blog model

--- core/blog/models.py
+++ core/blog/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+from core.accounts.models import User
+
+class Blog(models.Model):
+    object_id = models.CharField(max_length=11, null=True, blank=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=1000, null=True, blank=True)
+    body = models.TextField(null=False, blank=False)
+        
+    displayed_upvotes = models.PositiveIntegerField(default=0)
+    upvotes = models.PositiveIntegerField(default=0)
+    date_created = models.DateField(auto_now_add=True, null=True, blank=True)
+    datetime_created = models.DateTimeField(auto_now_add=True, null=True, blank=True)
