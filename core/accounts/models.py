8ae9ff42dc75701bc3af2871566fdf6ee784b315
Samkelo Kgosiemang <joymama078@gmail.com>
2021-07-15 12:21:22 +0200

    Add is artist check

--- core/accounts/models.py
+++ core/accounts/models.py
@@ -25,7 +25,7 @@ class User(AbstractUser):
     object_id = models.CharField(max_length=20, null=True, blank=True)
     is_fake_profile = models.BooleanField(default=False)
     is_verified = models.BooleanField(default=False)
-    has_server = models.BooleanField(default=False)
+    is_artist = models.BooleanField(default=False)
     gelt = models.IntegerField(default=1000)
     num_posts = models.PositiveIntegerField(default=0)
     username = LowercaseCharField(
