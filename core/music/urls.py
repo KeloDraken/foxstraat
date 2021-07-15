66f7a748f68cfb636888df9a0a0110472d0679f1
Samkelo Kgosiemang <joymama078@gmail.com>
2021-07-15 13:02:54 +0200

    Remove trailing song in music urls

--- core/music/urls.py
+++ core/music/urls.py
@@ -6,7 +6,7 @@ app_name = 'music'
 
 urlpatterns = [
     # Music
-    path('songs/add/', add_song, name='add-song'),
-    path('songs/explore/', music_chart, name='explore-music'),
-    path('songs/<song_id>/', get_song, name='get-song'),
+    path('add/', add_song, name='add-song'),
+    path('explore/', music_chart, name='explore-music'),
+    path('<song_id>/', get_song, name='get-song'),
 ]
