e6eb012e81417e6f2f3d045bf000d284819bc20a
Samkelo Kgosiemang <joymama078@gmail.com>
2021-07-16 16:32:15 +0200

    Change text in frontpage song card

--- core/marketplace/urls.py
+++ core/marketplace/urls.py
@@ -17,5 +17,5 @@ urlpatterns = [
     path('manage/', manage_listings, name='manage-listing'),
     path('delete/<listing_id>/', delete_listing, name='delete-listing'),
     path('buy/<listing_id>/', buy_template, name='buy-template'),
-    path('<listing_id>/', view_listing, name='view-listing'),
+    path('css/<listing_id>/', view_listing, name='view-listing'),
 ]
