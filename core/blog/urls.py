a16350e6480a394dc1b53141eebd03b4c7a0f6f7
Samkelo Kgosiemang <joymama078@gmail.com>
2021-07-16 12:35:58 +0200

    Create blog creation view

--- core/blog/urls.py
+++ core/blog/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from core.blog.views import get_blog
+from core.blog.views import add_blog_post, get_blog
 
 app_name = 'blog'
 
 urlpatterns = [
+    path('add/', add_blog_post, name='add-blog'),
     path('<blog_id>/', get_blog, name='get-blog'),
 ]
--- core/blog/views.py
+++ core/blog/views.py
@@ -1,13 +1,13 @@
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import redirect, render
+from django.shortcuts import get_object_or_404, redirect, render
 
 from core.forms import FormWithCaptcha
 
 from core.blog.forms import CreateBlogPostForm
 from core.blog.models import Blog
 
-from utils.helpers import ref_from_url
+from utils.helpers import object_id_generator, ref_from_url
 
 
 @login_required
@@ -28,8 +28,21 @@ def add_blog_post(request):
                 captcha_data = '...'
                 
             if not captcha_data == '':
-                
-                pass
+                if post_form.is_valid():
+                    post_form.save(commit=False)
+                    post_form.user = request.user
+
+                    object_id = object_id_generator(11, Blog)
+                    post_form.object_id = object_id
+                    post_form.save()
+
+                    return redirect('blog:get-blog', blog_id=object_id)
+
+                else:
+                    messages.error(
+                        request, 
+                        'An error occured. Couldn\'t save your post'
+                    )
             else:
                 messages.error(
                     request, 
@@ -49,5 +62,9 @@ def add_blog_post(request):
         )
 
 
-def get_blog(request):
-    pass
+def get_blog(request, blog_id):
+    blog = get_object_or_404(Blog, object_id=blog_id)
+    context = {
+        'blog': blog,
+    }
+    return render(request, 'views/blog/view_blog.html', context)
