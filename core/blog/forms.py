fdf022b0813105eac8189e3797e3c05b40c6aef4
Samkelo Kgosiemang <joymama078@gmail.com>
2021-07-16 12:41:57 +0200

    Add classes to blog creation form fields

--- core/blog/forms.py
+++ core/blog/forms.py
@@ -4,6 +4,28 @@ from core.blog.models import Blog
 
 
 class CreateBlogPostForm(forms.ModelForm):
+    title = forms.CharField(
+        max_length=1000, 
+        required=True, 
+        widget=forms.TextInput(
+            attrs={
+                'class': 'form-input',
+                'placeholder': 'Your post\'s title',
+                'autocomplete': 'off',
+                'autofocus': 'true',
+                'autocapitalize': 'off'
+            }
+        )
+    ) 
+    body = forms.CharField(required=True, widget=forms.Textarea(
+        attrs={
+            'cols': 100,
+            'class': 'form-input',
+            'placeholder': 'Write your blog\'s content here...',
+            'rows': 100,
+        }
+    ))
+
     class Meta:
         model = Blog
         fields = (
