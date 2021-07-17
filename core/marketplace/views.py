11c085ed061879885485b37dbd25379f90ddda6b
Samkelo Kgosiemang <joymama078@gmail.com>
2021-07-17 15:05:12 +0200

    Change template bought message

--- core/marketplace/views.py
+++ core/marketplace/views.py
@@ -202,7 +202,11 @@ def buy_template(request, listing_id):
             request.user.gelt-= template.price
             request.user.save()
 
-        messages.success(request, 'You\'ve successfully bought access to this template. Copy the CSS into the Customise Profile section of the edit profile page')
+        messages.success(
+            request, 
+            'You\'ve successfully bought access to this template. \
+            Copy the CSS into the Customise Profile section of \
+            the edit profile page or click Apply Styles')
         context = {
             'template': template
         }
