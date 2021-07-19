428c9b95ef627b55c6a0c5145d0cb30d70e9efd4
Samkelo Kgosiemang <joymama078@gmail.com>
2021-07-19 15:47:23 +0200

    Create LinkVote model

--- core/linkoff/models.py
+++ core/linkoff/models.py
@@ -8,6 +8,15 @@ class Link(models.Model):
     user = models.ForeignKey(User, on_delete=models.DO_NOTHING, null=True)
     title = models.CharField(max_length=200, null=False, blank=False)
     link = models.URLField(max_length=1000, null=False, blank=False)
+    upvotes = models.PositiveIntegerField(default=0)
+    downvotes = models.PositiveIntegerField(default=0)
+    score = models.IntegerField(default=0)
     date_created = models.DateField(auto_now_add=True)
     datetime_created = models.DateTimeField(auto_now_add=True)
 
+class LinkVote(models.Model):
+    user = models.ForeignKey(User, on_delete=models.DO_NOTHING, null=True)
+    link = models.ForeignKey(Link, on_delete=models.CASCADE)
+    value = models.IntegerField(default=0)
+    has_voted = models.BooleanField(default=False)
+    
