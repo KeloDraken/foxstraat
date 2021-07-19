dad999a91e05ab26047e0e79802e27e32d5e6c02
Samkelo Kgosiemang <joymama078@gmail.com>
2021-07-19 15:43:01 +0200

    Create link model

--- core/linkoff/models.py
+++ core/linkoff/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+from core.accounts.models import User
+
+
+class Link(models.Model):
+    object_id = models.CharField(max_length=11, null=False, blank=False)
+    user = models.ForeignKey(User, on_delete=models.DO_NOTHING, null=True)
+    title = models.CharField(max_length=200, null=False, blank=False)
+    link = models.URLField(max_length=1000, null=False, blank=False)
+    date_created = models.DateField(auto_now_add=True)
+    datetime_created = models.DateTimeField(auto_now_add=True)
+
