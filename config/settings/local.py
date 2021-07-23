1a8cde9632972a89f23476cab6c69ee95ffc74c6
Samkelo Kgosiemang <joymama078@gmail.com>
2021-07-23 19:52:18 +0200

    Add secret key

--- config/settings/local.py
+++ config/settings/local.py
@@ -1,17 +1,33 @@
+import os
+import sys
+import dj_database_url
+
 from config.settings.base import *
 
-SECRET_KEY = 'THIS IS NOT THE SECRET KEY'
+from django.core.management.utils import get_random_secret_key
 
-DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", get_random_secret_key())
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+DEBUG = os.getenv("DEBUG", "False") == "True"
+
+DEVELOPMENT_MODE = os.getenv("DEVELOPMENT_MODE", "False") == "True"
+
+ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS", "127.0.0.1,localhost").split(",")
+
+if DEVELOPMENT_MODE is True:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
+        }
     }
-}
+elif len(sys.argv) > 0 and sys.argv[1] != 'collectstatic':
+    if os.getenv("DATABASE_URL", None) is None:
+        raise Exception("DATABASE_URL environment variable not defined")
+    DATABASES = {
+        "default": dj_database_url.parse(os.environ.get("DATABASE_URL")),
+    }
 
 REST_FRAMEWORK = {
     'DEFAULT_PARSER_CLASSES': [
@@ -25,7 +41,7 @@ REST_FRAMEWORK = {
 
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'static-root'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_DIRS = [
     BASE_DIR /'static'
 ]
