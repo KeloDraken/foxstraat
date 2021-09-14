7dae91d7818bf22334c6d9ca5a5a180791bd3c45
Joy Mama <jmama021@student.wethinkcode.co.za>
2021-09-14 15:56:20 +0200

    Remove is_mobile check

--- foxstraat/core/views.py
+++ foxstraat/core/views.py
@@ -1,7 +1,7 @@
 from django.contrib import messages
 from django.shortcuts import redirect, render
 
-from foxstraat.utils.helpers import is_mobile, object_id_generator
+from foxstraat.utils.helpers import object_id_generator
 from foxstraat.core.models import Feedback, News, Privacy, Rules, Terms
 
 
@@ -25,49 +25,30 @@ def add_feedback(request):
             return redirect("about")
 
 
-def index(request):
-    return redirect("bulletin:frontpage")
-
-
 def news(request):
     news_ = News.objects.all().order_by("-datetime_created")
     context = {"heading": "Foxstraat News", "news": news_}
-    if not is_mobile(request):
-        return render(request, "views/blog/news.html", context)
-    else:
-        return render(request, "mobile/views/legal/news.html", context)
+    return render(request, "views/blog/news.html", context)
 
 
 def about(request):
-    if not is_mobile(request):
-        add_feedback(request)
-        return render(request, "views/index.html", context={"page": "about"})
-    else:
-        return render(request, "mobile/views/about.html")
+    add_feedback(request)
+    return render(request, "views/index.html", context={"page": "about"})
 
 
 def terms(request):
     news_ = Terms.objects.all()
     context = {"heading": "Terms of Service", "news": news_}
-    if not is_mobile(request):
-        return render(request, "views/blog/news.html", context)
-    else:
-        return render(request, "mobile/views/legal/news.html", context)
+    return render(request, "views/blog/news.html", context)
 
 
 def privacy(request):
     news_ = Privacy.objects.all()
     context = {"heading": "Privacy Policy", "news": news_}
-    if not is_mobile(request):
-        return render(request, "views/blog/news.html", context)
-    else:
-        return render(request, "mobile/views/legal/news.html", context)
+    return render(request, "views/blog/news.html", context)
 
 
 def rules(request):
     news_ = Rules.objects.all()
     context = {"heading": "Foxstraat Rules", "news": news_}
-    if not is_mobile(request):
-        return render(request, "views/blog/news.html", context)
-    else:
-        return render(request, "mobile/views/legal/news.html", context)
+    return render(request, "views/blog/news.html", context)
