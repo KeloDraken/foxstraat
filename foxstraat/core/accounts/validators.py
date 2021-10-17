570c7a9f4bea01fccddd7cd1ec4ed3aa274c13f3
Kelo <joymama078@gmail.com>
2021-10-17 15:11:26 +0200

    Update email validator to only accept WTC email

--- foxstraat/core/accounts/validators.py
+++ foxstraat/core/accounts/validators.py
@@ -7,6 +7,7 @@ from django.utils.translation import gettext_lazy as _
 
 @deconstructible
 class UnicodeEmailValidator(validators.RegexValidator):
-    regex = r"(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$)"
-    message = _("Enter a valid email address.")
+    regex = r"(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.wethinkcode.co.za+$)"
+    # regex = r"(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$)"
+    message = _("Enter a valid WeThinkCode email address.")
     flags = 0
